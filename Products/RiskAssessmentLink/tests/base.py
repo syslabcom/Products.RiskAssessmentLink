9e1618eaf284cae84ebfd4b9e1c514d5d0ef1ffe
do3cc <do3cc>
2009-06-16 17:40:53 +0000

    Made tests working and removed a test that didnt test the product itself

--- Products/RiskAssessmentLink/tests/base.py
+++ Products/RiskAssessmentLink/tests/base.py
@@ -2,22 +2,39 @@ from Testing import ZopeTestCase as ztc
 from Products.PloneTestCase import PloneTestCase as ptc
 import gocept.linkchecker
 
-ztc.installProduct('RiskAssessmentLink')
-ztc.installProduct('ATVocabularyManager')
-ztc.installPackage('gocept.linkchecker')
-ztc.installProduct('ZCatalog')
+from Products.Five import fiveconfigure, zcml
+from Products.PloneTestCase import layer
 
-ptc.setupPloneSite(products=['RiskAssessmentLink', 'ATVocabularyManager'])
+SiteLayer = layer.PloneSite
+
+class RALinkLayer(SiteLayer):
+    @classmethod
+    def setUp(cls):
+        ztc.installProduct('ATVocabularyManager')
+        ztc.installProduct('RiskAssessmentLink')
+        ztc.installProduct('ZCatalog')
+        fiveconfigure.debug_mode = True
+
+        import gocept.linkchecker
+        zcml.load_config('configure.zcml', gocept.linkchecker)
+        fiveconfigure.debug_mode = False
+
+        ptc.setupPloneSite(products=['RiskAssessmentLink', 'ATVocabularyManager'])
+        ztc.installPackage('gocept.linkchecker')
+        SiteLayer.setUp()
+
 
 class RiskAssessmentLinkTestCase(ptc.PloneTestCase):
+    layer = RALinkLayer
     pass
-    
+
 class RiskAssessmentLinkFunctionalTestCase(ptc.FunctionalTestCase):
     """We use this class for functional integration tests that use
     doctest syntax. Again, we can put basic common utility or setup
     code in here.
     """
-    
+    layer = RALinkLayer
+
     class Session(dict):
         def set(self, key, value):
             self[key] = value
